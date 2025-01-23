b92274af7711caf4529dbfc0baedb4a8cd58c6de
Zack <zack.r.clifford@gmail.com>
2025-01-22 21:24:48 -0700

    toString overrides

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -76,4 +76,12 @@ public class ChessPiece {
     public int hashCode() {
         return Objects.hash(teamColor, pieceType);
     }
+
+    @Override
+    public String toString() {
+        return "ChessPiece{" +
+                "teamColor=" + teamColor +
+                ", pieceType=" + pieceType +
+                '}';
+    }
 }
