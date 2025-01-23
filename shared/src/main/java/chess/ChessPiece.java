64a3103c02e8c756499298a18cc9b20f17b8d817
Zack <zack.r.clifford@gmail.com>
2025-01-22 21:22:46 -0700

    equals and hashCode overrides

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,6 +1,7 @@
 package chess;
 
 import java.util.Collection;
+import java.util.Objects;
 
 /**
  * Represents a single chess piece
@@ -60,5 +61,19 @@ public class ChessPiece {
             case KNIGHT -> new KnightMoveCalculator();
         };
         return calculator.pieceMoves(board, myPosition);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessPiece that = (ChessPiece) o;
+        return teamColor == that.teamColor && pieceType == that.pieceType;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(teamColor, pieceType);
     }
 }
