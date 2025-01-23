07278cd969a1da303458df1d63d9404400bf67db
Zack <zack.r.clifford@gmail.com>
2025-01-22 21:35:20 -0700

    working with the interface made me want to perish

--- shared/src/main/java/chess/PieceMovesCalculator.java
+++
@@ -1,9 +0,0 @@
-package chess;
-import java.util.Collection;
-public interface PieceMovesCalculator {
-    Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition);
-//    pieceMoves(board: ChessBoard, position: ChessPosition) {
-//        return Collection<ChessMove> monkey;
-//    }
-}
-
