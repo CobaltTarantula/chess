2b408bacad09dedd84b3a84dc9b16ae1922227cf
Zack <zack.r.clifford@gmail.com>
2025-01-26 14:24:03 -0700

    testing ideas in ChessBoard

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -56,6 +56,7 @@ public class ChessBoard {
 
     private void setBlack(){
         squares[7][0] = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.ROOK);
+        // addPiece(squares[7][0], ChessPiece.PieceType.KNIGHT);
         squares[7][1] = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KNIGHT);
         squares[7][2] = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP);
         squares[7][3] = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.QUEEN);
