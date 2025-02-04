3358631fa543268bca0aa712800d7cec4f2a8201
Zack <zack.r.clifford@gmail.com>
2025-02-03 23:48:49 -0700

    safeMove implemented but need to fill out line for actual piece movement

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -60,12 +60,28 @@ public class ChessGame {
         // parse through possible moves to id legal moves
         for(ChessMove move : possMoves){
             // if safe move (doesn't put King in check) then add to legalMoves
-            if(/*safe move*/){
+            if(safeMove(move)){
                 legalMoves.add(move);
             }
         }
 
         return legalMoves;
+    }
+
+    private boolean safeMove(ChessMove move){
+        ChessBoard testBoard = new ChessBoard();
+        for(int i = 1; i <= 8; i++){
+            for(int j = 1; j <= 8; j++){
+                ChessPosition pos = new ChessPosition(i, j);
+                ChessPiece piece = currBoard.getPiece(pos);
+                if(piece!=null){
+                    testBoard.addPiece(pos, new ChessPiece(piece.getTeamColor(), piece.getPieceType()));
+                }
+            }
+        }
+        TeamColor pieceColor = testBoard.getPiece(move.getStartPosition()).getTeamColor();
+        // move the piece
+        return isInCheck(pieceColor);
     }
 
     /**
