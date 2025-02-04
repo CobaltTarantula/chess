29cc8baee3fa8d59041d17576450f93df36c8877
Zack <zack.r.clifford@gmail.com>
2025-02-03 23:06:37 -0700

    added this in setTeam

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -27,7 +27,7 @@ public class ChessGame {
      * @param team the team whose turn it is
      */
     public void setTeamTurn(TeamColor team) {
-        currTurn = team;
+        this.currTurn = team;
     }
 
     /**
