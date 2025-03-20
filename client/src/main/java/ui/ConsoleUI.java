9d1e640965bbe029833556ec13818c687c05d253
Zack <zack.r.clifford@gmail.com>
2025-03-19 19:51:27 -0600

    create outline for future work on Phase 5

---
+++ client/src/main/java/ui/ConsoleUI.java
@@ -0,0 +1,45 @@
+package ui;
+
+// imports here
+import client.ServerFacade;
+
+public class ConsoleUI {
+    private final ServerFacade facade;
+    public ConsoleUI(ServerFacade facade, ServerFacade facade1){
+
+        this.facade = facade1;
+    }
+    private void preLogin(){
+
+    }
+    private void postLogin(){
+
+    }
+    private void help(){
+
+    }
+    private void quit(){
+
+    }
+    private void login(){
+
+    }
+    private void register(){
+
+    }
+    private void logout(){
+
+    }
+    private void createGame(){
+
+    }
+    private void listGames(){
+
+    }
+    private void playGame(){
+
+    }
+    private void observeGame(){
+
+    }
+}
